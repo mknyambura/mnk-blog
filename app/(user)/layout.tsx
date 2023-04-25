2a20975be097f39fe7f53e2505a89a88ad509d90
mknyambura <kariuki.mercie@gmail.com>
2023-04-26 01:48:55 +0300

    update header component and styling

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -1,5 +1,7 @@
+import Header from "@/components/Header"
 import "../../app/globals.css"
 import { Inter } from 'next/font/google'
+import Banner from "@/components/Banner"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +17,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {/* Header */}
+        <Header/>
+        {/* Banner */}
+        <Banner/>
+        {children}
+      </body>
     </html>
   )
 }
